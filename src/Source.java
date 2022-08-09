c9fdfbeca907e37834be14fd9d9e115bae6ff68f
AliaksandrKryvapust <alexandrkryvopust@gmail.com>
2022-08-09 11:31:25 +0200

    Final

---
+++ src/Source.java
@@ -0,0 +1,9 @@
+public class Source {
+    public static void main(String[] args) {
+    int n = 100;
+        System.out.println(nthEven(n)==198);
+    }
+    public static int nthEven(int n) {
+        return n*2-2;
+    }
+}
